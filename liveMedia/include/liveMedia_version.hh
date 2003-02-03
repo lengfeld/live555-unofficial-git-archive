f9ebda92333c08ad5f611e4a99701ef6544ef431
Live Networks, Inc <invalid@example.com>
2003-02-03 00:00:00 +0000

    unpack live.2003.02.03.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ liveMedia/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2003 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2003.02.03"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1044230400
+
+#endif
