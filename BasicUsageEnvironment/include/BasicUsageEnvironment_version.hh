d7ccd0a21d2e10b5d84e16efb4a777f4cb518d2a
Live Networks, Inc <invalid@example.com>
2004-02-03 00:00:00 +0000

    unpack live.2004.02.03.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "BasicUsageEnvironment" library
+// Copyright (c) 1996-2004 Live Networks, Inc.  All rights reserved.
+
+#ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
+#define _BASICUSAGEENVIRONMENT_VERSION_HH
+
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2004.02.03"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1075766400
+
+#endif
