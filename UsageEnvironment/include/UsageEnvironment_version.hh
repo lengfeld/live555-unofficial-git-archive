bbaa37fe5f9b79670856374d3a2cfecdf6b4566c
Live Networks, Inc <invalid@example.com>
2004-12-23 00:00:00 +0000

    unpack live.2004.12.23.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "UsageEnvironment" library
+// Copyright (c) 1996-2004 Live Networks, Inc.  All rights reserved.
+
+#ifndef _USAGEENVIRONMENT_VERSION_HH
+#define _USAGEENVIRONMENT_VERSION_HH
+
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2004.12.23"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1103760000
+
+#endif
