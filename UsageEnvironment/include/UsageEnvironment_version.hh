ed3c36f81da252a5b288c79b0a90e50d88747cca
Live Networks, Inc <invalid@example.com>
2005-05-26 00:00:00 +0000

    unpack live.2005.05.26a.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "UsageEnvironment" library
+// Copyright (c) 1996-2005 Live Networks, Inc.  All rights reserved.
+
+#ifndef _USAGEENVIRONMENT_VERSION_HH
+#define _USAGEENVIRONMENT_VERSION_HH
+
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2005.05.26"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1117065600
+
+#endif
---
+++ groupsock/include/groupsock_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "groupsock" library
+// Copyright (c) 1996-2005 Live Networks, Inc.  All rights reserved.
+
+#ifndef _GROUPSOCK_VERSION_HH
+#define _GROUPSOCK_VERSION_HH
+
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2005.05.26"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1117065600
+
+#endif
