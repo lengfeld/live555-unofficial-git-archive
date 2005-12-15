85675f59f12f97bc066078b4525304bc9e326aac
Live Networks, Inc <invalid@example.com>
2005-12-15 00:00:00 +0000

    unpack live.2005.12.15.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ liveMedia/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2005 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2005.12.15"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1134604800
+
+#endif
