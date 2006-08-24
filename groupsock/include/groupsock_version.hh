e94d4ca51b746c5011957f4309d89f047da6bc2f
Live Networks, Inc <invalid@example.com>
2006-08-24 00:00:00 +0000

    unpack live.2006.08.24.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ groupsock/include/groupsock_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "groupsock" library
+// Copyright (c) 1996-2006 Live Networks, Inc.  All rights reserved.
+
+#ifndef _GROUPSOCK_VERSION_HH
+#define _GROUPSOCK_VERSION_HH
+
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2006.08.24"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1156377600
+
+#endif
---
+++ liveMedia/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2006 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2006.08.24"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1156377600
+
+#endif
