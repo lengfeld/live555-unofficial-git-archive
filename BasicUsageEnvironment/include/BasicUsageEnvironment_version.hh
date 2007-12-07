342365b3681139fe61d585fbc25aad8c7ca2bce5
Live Networks, Inc <invalid@example.com>
2007-12-07 00:00:00 +0000

    unpack live.2007.12.07.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "BasicUsageEnvironment" library
+// Copyright (c) 1996-2007 Live Networks, Inc.  All rights reserved.
+
+#ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
+#define _BASICUSAGEENVIRONMENT_VERSION_HH
+
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2007.12.06"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1196899200
+
+#endif
---
+++ groupsock/include/groupsock_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "groupsock" library
+// Copyright (c) 1996-2007 Live Networks, Inc.  All rights reserved.
+
+#ifndef _GROUPSOCK_VERSION_HH
+#define _GROUPSOCK_VERSION_HH
+
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2007.12.06"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1196899200
+
+#endif
