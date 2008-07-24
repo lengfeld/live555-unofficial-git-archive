f20797cf4140d1892046a14c875d6315ad92390c
Live Networks, Inc <invalid@example.com>
2008-07-24 00:00:00 +0000

    unpack live.2008.07.24.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "UsageEnvironment" library
+// Copyright (c) 1996-2008 Live Networks, Inc.  All rights reserved.
+
+#ifndef _USAGEENVIRONMENT_VERSION_HH
+#define _USAGEENVIRONMENT_VERSION_HH
+
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2008.07.23"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1216771200
+
+#endif
