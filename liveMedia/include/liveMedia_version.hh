06fc45345244906a82c6317b9677fc95af56b1a1
Live Networks, Inc <invalid@example.com>
2009-06-02 00:00:00 +0000

    unpack live.2009.06.02.tar.gz from sources.openembedded.org
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ liveMedia/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2009 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2009.06.02"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1243900800
+
+#endif
