1814035f55102e79f21068645fc55227c0a9a904
Live Networks, Inc <invalid@example.com>
2011-05-25 00:00:00 +0000

    unpack live.2011.05.25.tar.gz from tor
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ groupsock/include/groupsock_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "groupsock" library
+// Copyright (c) 1996-2011 Live Networks, Inc.  All rights reserved.
+
+#ifndef _GROUPSOCK_VERSION_HH
+#define _GROUPSOCK_VERSION_HH
+
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2011.05.25"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1306281600
+
+#endif
