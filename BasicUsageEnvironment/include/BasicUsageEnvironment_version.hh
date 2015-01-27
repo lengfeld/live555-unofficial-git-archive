690d8efd634be60071b8efff2eb9c83d056775cd
Live Networks, Inc <invalid@example.com>
2015-01-27 00:00:00 +0000

    unpack live.2015.01.27.tar.gz from mirror.sobukus.de
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "BasicUsageEnvironment" library
+// Copyright (c) 1996-2015 Live Networks, Inc.  All rights reserved.
+
+#ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
+#define _BASICUSAGEENVIRONMENT_VERSION_HH
+
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2015.01.27"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1422316800
+
+#endif
