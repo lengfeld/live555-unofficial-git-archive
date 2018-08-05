abe8237ad9b3b84b5dc831ea7fe09a494539cb2e
Live Networks, Inc <invalid@example.com>
2018-08-05 00:00:00 +0000

    unpack live.2018.08.05.tar.gz from uni-hamburg.de
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ liveMedia/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2018 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2018.08.05"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1533427200
+
+#endif
