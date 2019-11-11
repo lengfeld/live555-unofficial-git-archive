6f0f0ab80ba438f1d0d0840c0b03af98bfd63b5c
Live Networks, Inc <invalid@example.com>
2019-11-11 00:00:00 +0000

    unpack live.2019.11.11.tar.gz from uni-hamburg.de
    Copyright (c), Live Networks, Inc.  All rights reserved

---
+++ BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "BasicUsageEnvironment" library
+// Copyright (c) 1996-2019 Live Networks, Inc.  All rights reserved.
+
+#ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
+#define _BASICUSAGEENVIRONMENT_VERSION_HH
+
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2019.11.11"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1573430400
+
+#endif
