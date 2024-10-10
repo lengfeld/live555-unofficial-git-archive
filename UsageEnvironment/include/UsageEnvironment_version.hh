f0830d5957895b12b1446f1c1ce53fbe2e5b394e
Live Networks, Inc <invalid@example.com>
2024-10-10 00:00:00 +0000

    unpack live.2024.10.10.tar.gz
    Copyright (c), Live Networks, Inc.  All rights reserved.
    
    Changelog:
    
    - Fixed a stack-use-after-return bug in the RTSP server implementation.
      (Thanks to Sangjun Park for reporting this.)

--- UsageEnvironment/include/UsageEnvironment_version.hh
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _USAGEENVIRONMENT_VERSION_HH
 #define _USAGEENVIRONMENT_VERSION_HH
 
-#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2024.10.09"
-#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1728432000
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2024.10.10"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1728518400
 
 extern char const* const UsageEnvironmentLibraryVersionStr;
 extern int const UsageEnvironmentLibraryVersionInt;
--- groupsock/include/groupsock_version.hh
+++ groupsock/include/groupsock_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _GROUPSOCK_VERSION_HH
 #define _GROUPSOCK_VERSION_HH
 
-#define GROUPSOCK_LIBRARY_VERSION_STRING	"2024.10.09"
-#define GROUPSOCK_LIBRARY_VERSION_INT		1728432000
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2024.10.10"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1728518400
 
 extern char const* const groupsockLibraryVersionStr;
 extern int const groupsockLibraryVersionInt;
